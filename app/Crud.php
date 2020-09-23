a85e83b9fadc214cb88e07af7e1780cd37367a46
StefKari <59152638+StefKari@users.noreply.github.com>
2020-09-23 23:01:08 +0200

    Crud.php

--- app/Crud.php
+++ app/Crud.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Crud extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
     protected $table="cruds";
+
+    protected $fillable = [
+        'body',
+        'created_at',
+        'updated_at',
+    ];
 }
